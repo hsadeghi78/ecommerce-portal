fcd13ad6975595a22cebabeea73221c1fc09abad
h.sadeghi <hsadeghi78@gmail.com>
2023-12-29 14:59:43 +0330

    setup material design success

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import locale from '@angular/common/locales/en';
@@ -16,15 +16,22 @@ import { httpInterceptorProviders } from 'app/core/interceptor/index';
 import FindLanguageFromKeyPipe from 'app/shared/language/find-language-from-key.pipe';
 import { AppRoutingModule } from './app-routing.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { NgbDateDayjsAdapter } from './config/datepicker-adapter';
 import { fontAwesomeIcons } from './config/font-awesome-icons';
 import MainComponent from './layouts/main/main.component';
 import MainModule from './layouts/main/main.module';
 import { AppPageTitleStrategy } from './app-page-title-strategy';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     AppRoutingModule,
     // Set this to true to enable service worker (PWA)
@@ -32,6 +39,11 @@ import { AppPageTitleStrategy } from './app-page-title-strategy';
     HttpClientModule,
     MainModule,
     TranslationModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [
     Title,
@@ -40,8 +52,10 @@ import { AppPageTitleStrategy } from './app-page-title-strategy';
     FindLanguageFromKeyPipe,
     httpInterceptorProviders,
     { provide: TitleStrategy, useClass: AppPageTitleStrategy },
+    provideAnimations(),
   ],
   bootstrap: [MainComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
   constructor(applicationConfigService: ApplicationConfigService, iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig) {
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,13 +6,18 @@ import { takeUntil } from 'rxjs/operators';
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, MatSlideToggleModule, MatSliderModule, FormsModule, MatFormFieldModule, MatInputModule],
 })
 export default class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
